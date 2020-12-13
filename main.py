7b42584818e07be97e911a78979daf7b99b7a54f
Michael Best <michael.best@gmail.com>
2020-12-12 17:51:33 -0700

    update documentation - add more return types - update get_snapshots to not add the first snapshot/machine to the list of possible ids, which also simplifies determining which snapshots to delete

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ session = virtualbox.Session()
 
 
 def check_arguments():
+    """
+
+
+    :rtype : str
+    """
     if len(sys.argv) < 2:
         print("Not enough argument provided")
         return 1
     elif not isinstance(sys.argv[1], str):
-        print("Provided name is not istance of string")
+        print("Provided name is not a string")
         return 1
     return sys.argv[1]
 
@@ -21,12 +26,18 @@ def get_snapshots(snapshot, snapshots=None):
     """
 
 
-    :type snapshots: list|None
+
     :type snapshot: IMachine
+    :type snapshots: list|None
+    :rtype : list
     """
-    if not snapshots:
+    if snapshots is None:
         snapshots = []
-    snapshots.append(snapshot.id_p)
+        # Create an empty list when one doesn't exist
+        # Don't add the root snapshot to the list
+    else:
+        # Add the current child snapshot id to the list
+        snapshots.append(snapshot.id_p)
     if hasattr(snapshot, 'children'):
         for child in snapshot.children:
             get_snapshots(child, snapshots)
@@ -36,8 +47,10 @@ def get_snapshots(snapshot, snapshots=None):
 def snapshot_info(machine, snapshot_id):
     """
 
-    :type snapshot_id: str
+
     :type machine: IMachine
+    :type snapshot_id: str
+    :rtype : list
     """
     snapshot_obj = machine.find_snapshot(snapshot_id)
     date = datetime.fromtimestamp(snapshot_obj.time_stamp / 1000.0).strftime("%d-%m-%Y %H:%M:%S")
@@ -47,8 +60,10 @@ def snapshot_info(machine, snapshot_id):
 def delete_snapshots(machine, num_snapshots_to_keep=4):
     """
 
+
     :type machine: IMachine
     :type num_snapshots_to_keep: int
+    :rtype : None
     """
 
     root_snapshot = machine.find_snapshot('')
@@ -56,7 +71,7 @@ def delete_snapshots(machine, num_snapshots_to_keep=4):
 
     if len(snapshots) > num_snapshots_to_keep:
         highest_snapshot_to_delete = len(snapshots) - num_snapshots_to_keep
-        snapshots_to_delete = snapshots[1:highest_snapshot_to_delete]
+        snapshots_to_delete = snapshots[:highest_snapshot_to_delete]
         if snapshots_to_delete:
             try:
                 print()
@@ -74,7 +89,9 @@ def delete_snapshots(machine, num_snapshots_to_keep=4):
 def create_snapshot(machine):
     """
 
+
     :type machine: IMachine
+    :rtype : int
     """
     vm_initial_status = 1  # zero means powered on
 
@@ -91,7 +108,7 @@ def create_snapshot(machine):
 
     if vm_initial_status:
         if machine.session_state == virtualbox.library.SessionState(2):  # SessionState(2) = Locked
-            # The first IF check wheter the machine is in locked session, the second one checks if
+            # The first IF check whether the machine is in locked session, the second one checks if
             # the session is locked
             if session.state == virtualbox.library.SessionState(2):
                 session.unlock_machine()
@@ -113,8 +130,9 @@ def snapshot_to_string(machine, snapshot):
     """
 
 
-    :type snapshot: str
     :type machine: IMachine
+    :type snapshot: str
+    :rtype : str
     """
     return ', '.join([str(elem) for elem in snapshot_info(machine, snapshot)])
 
@@ -122,6 +140,8 @@ def snapshot_to_string(machine, snapshot):
 def print_snapshots_info(machine):
     """
 
+
+    :rtype : None
     :type machine: IMachine
     """
     root_snapshot = machine.find_snapshot('')
@@ -129,7 +149,7 @@ def print_snapshots_info(machine):
         snapshots = get_snapshots(root_snapshot)
         print()
         print(machine.name, 'snapshots:')
-        for snapshot in snapshots[1:]:
+        for snapshot in snapshots:
             print(snapshot_to_string(machine, snapshot))
 
 
